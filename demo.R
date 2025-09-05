3c704d23101136e23e6db1b26a0f50e56c9c9246
c-belbas <c.belbas@uwinnipeg.ca>
2025-09-05 15:29:01 -0500

    added mpg plot

--- demo.R
+++ demo.R
@@ -1,1 +1,15 @@
 rm(list = ls())
+library(ggplot2)
+install.packages("ggplot2")
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+avg_mpg <- mpg %>%
+  group_by(class) %>%
+  summarise(avg_hwy_mpg = mean(hwy, na.rm = TRUE))
+ggplot(avg_mpg, aes(y=class, x=avg_hwy_mpg))+
+  geom_col(fill="lightblue")+
+  labs(title = "Average Highway MPG by Vehicle Class",
+       y = "Vehicle Class",
+       x = "Average Highway MPG") +
+  theme_minimal()
